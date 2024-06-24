26880c04795b4c0f36de22e60ace52220ef9061c
Yehang Han <yehang.han@cytiva.com>
2024-06-24 09:47:18 +0800

    add new ByocPromo component

---
+++ app/byoc/ByocPromo.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import * as FEAAS from '@sitecore-feaas/clientside/react';
+
+interface ByocPromoProps {
+    imageUrl: string;
+    text: string;
+    url: string;
+    urlText: string;
+}
+
+export const ByocPromo = (props: ByocPromoProps): JSX.Element => {
+    return (
+      <div className="component promo col-12 link-button">
+        <div className="component-content">
+            <div className="field-promoicon">
+            {props.imageUrl && <img alt="" width="2000" height="1116" src={props.imageUrl} />}
+            </div>
+            <div className="promo-text">
+            <div>
+                <div className="field-promotext">
+                <div>
+                    <p>{props.text}</p>
+                </div>
+                </div>
+            </div>
+            {props.url && (
+                <div className="field-promolink">
+                <a href={props.url} title={props.urlText} target="_blank">
+                    {props.urlText}
+                </a>
+                </div>
+            )}
+            </div>
+        </div>
+      </div>
+    );
+};
+
+FEAAS.External.registerComponent(ByocPromo, {
+    name: 'ByocPromo',
+    title: 'BYOC Promo',
+    description: 'BYOC Promo',
+    thumbnail: 'https://cdn.cytivalifesciences.com.cn/api/public/content/digi-33587-original',
+    group: 'Cytiva-Design-Book',
+    required: ['text', 'imageUrl', 'url'],
+    properties: {
+      imageUrl: {
+        type: 'string',
+      },
+      text: {
+        type: 'string',
+      },
+      url: {
+        type: 'string',
+      },
+      urlText: {
+        type: 'string',
+      },
+    },
+    ui: {
+      text: {
+        'ui:autofocus': true,
+        'ui:emptyValue': '',
+        'ui:placeholder': 'Write a great Promo text 2',
+      },
+    },
+});
--- app/byoc/index.client.tsx
+++ app/byoc/index.client.tsx
@@ -26,3 +26,4 @@ export default (props: any) => {
 //import "./ExampleAugmentedComponent";
 
 import "./ByocCytiva";
+import "./ByocPromo";
